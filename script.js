732a3d4ec66a35e7624c649e49b9bf200c37b991
Brent Cleary <brent.cleary@gmail.com>
2024-05-05 13:55:12 -0700

    FirstCommit_HTML_for_Google_Extension

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('revealButton').addEventListener('click', function() {
+  var hiddenContent = document.getElementById('hiddenContent');
+  hiddenContent.classList.toggle('hidden');
+});
